8c758b6affa392964dd5e9bf5f05d6dfe6da20b5
nigelhughes552 <nigel@theoldpo.plus.com>
2019-07-23 23:44:19 +0100

    Edits

---
+++ layouts/default.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+      <v-list>
+        <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>web</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>remove</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "apps",
+          title: "Menu",
+          to: "/"
+        },
+        {
+          icon: "bubble_chart",
+          title: "About",
+          to: "/about"
+        },
+        {
+          icon: "person",
+          title: "Cup Draws",
+          to: "/cupDraws"
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Banbury & District Aunt Sally League"
+    };
+  }
+};
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <h3>Welcome to the Banbury &amp; District Aunt Sally Website</h3>
+      <h5>For All your News &amp; Results</h5>
+      <p>
+        The League has been in existence for over 40 years and is well supported by over 50 teams in and around the Banbury area.
+      <p>The League is divided into 6 Divisions</p>  
+       <p>League matches are played on Thursday evenings</p> 
+       <p>
+        Competition matches are played throughout the season on Tuesday evenings, with the exception
+        of the Singles &amp; Pairs Cups which are played on Thursday evinings at the half way point of the season.
+      </p>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
